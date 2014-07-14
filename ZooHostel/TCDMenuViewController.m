f6effc452604e7142e898dd9cbb364c9623e4fb9
Dmitrii Doroschuk <Dmitriy.doroschuk@e-legion.com>
2014-07-15 01:57:25 +0400

    top of side menu made like parallax

--- ZooHostel/TCDMenuViewController.m
+++ ZooHostel/TCDMenuViewController.m
@@ -13,9 +13,14 @@ static NSString * const kMainSegueIdentifier = @"MainSegueIdentifier";
 static NSString * const kContactsSegueIdentifier = @"ContactsSegueIdentifier";
 static NSString * const kPhotoSegueIdentifier = @"PhotoSegueIdentifier";
 
+static CGFloat const kTableViewYOrigin = 100.f;
+static CGFloat const kTopImageViewHeight = 139.f;
+
 @interface TCDMenuViewController () <UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) UIImageView *headerImageView;
+@property (assign, nonatomic) CGFloat headerImageYOffset;
 @property (copy, nonatomic) NSArray *dataSource;
 @property (copy, nonatomic) NSArray *seguesSource;
 @property (strong, nonatomic) NSIndexPath *selectedIndexPath;
@@ -35,6 +40,24 @@ static NSString * const kPhotoSegueIdentifier = @"PhotoSegueIdentifier";
     
     self.selectedIndexPath = [NSIndexPath indexPathForRow:0 inSection:0];
     // Do any additional setup after loading the view.
+    
+    // Create an empty table header view with small bottom border view
+    UIView *tableHeaderView = [[UIView alloc] initWithFrame: CGRectMake(0.0, 0.0, self.view.frame.size.width, 120.0)];
+    tableHeaderView.backgroundColor = [UIColor clearColor];
+    UIView *blackBorderView = [[UIView alloc] initWithFrame: CGRectMake(0.0, 119.0, self.view.frame.size.width, 1.0)];
+    blackBorderView.backgroundColor = [UIColor colorWithRed: 0.0 green: 0.0 blue: 0.0 alpha: 0.8];
+    [tableHeaderView addSubview: blackBorderView];
+    
+    _tableView.tableHeaderView = tableHeaderView;
+    
+    // Create the underlying imageview and offset it
+    self.headerImageYOffset = -30.0;
+    self.headerImageView = [[UIImageView alloc] initWithImage: [UIImage imageNamed: @"menu_top"]];
+    CGRect headerImageFrame = self.headerImageView.frame;
+    headerImageFrame.origin.x -= 30.f;
+    headerImageFrame.origin.y = self.headerImageYOffset;
+    self.headerImageView.frame = headerImageFrame;
+    [self.view insertSubview: self.headerImageView belowSubview: self.tableView];
 }
 
 #pragma mark - UITableView
@@ -51,7 +74,7 @@ static NSString * const kPhotoSegueIdentifier = @"PhotoSegueIdentifier";
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
     cell.textLabel.text = self.dataSource[indexPath.row];
-    cell.backgroundColor = [UIColor clearColor];
+    cell.backgroundColor = [UIColor darkGrayColor];
     cell.textLabel.textColor = [UIColor whiteColor];
     if ([indexPath isEqual:self.selectedIndexPath]) {
         [tableView selectRowAtIndexPath:indexPath animated:NO scrollPosition:UITableViewScrollPositionNone];
@@ -72,11 +95,24 @@ static NSString * const kPhotoSegueIdentifier = @"PhotoSegueIdentifier";
 
 #pragma mark - UIScrollViewDelegate
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    if (scrollView.contentOffset.y < 0) {
-        scrollView.contentOffset = CGPointMake(scrollView.contentOffset.x, 0);
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat scrollOffset = scrollView.contentOffset.y;
+    CGRect headerImageFrame = self.headerImageView.frame;
+    
+    if (scrollOffset <= -70) {
+        scrollView.contentOffset = CGPointMake(scrollView.contentOffset.x, -70);
+        return;
     }
+    
+    if (scrollOffset < 0) {
+        // Adjust image proportionally
+        headerImageFrame.origin.y = self.headerImageYOffset - ((scrollOffset / 3));
+    } else {
+        // We're scrolling up, return to normal behavior
+        headerImageFrame.origin.y = self.headerImageYOffset - scrollOffset / 3;
+    }
+    
+    self.headerImageView.frame = headerImageFrame;
 }
 
 #pragma mark - Navigation
