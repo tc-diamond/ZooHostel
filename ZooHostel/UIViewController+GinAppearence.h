b2beca8a861b2cc45322b143d8fc2ce7d50440ca
Dmitrii Doroschuk <Dmitriy.doroschuk@e-legion.com>
2014-07-23 00:03:15 +0400

    EXPhotoViewer added

---
+++ ZooHostel/UIViewController+GinAppearence.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+GinAppearence.h
+//  ZooHostel
+//
+//  Created by Dmitri Doroschuk on 21.07.14.
+//  Copyright (c) 2014 Dmitri Doroschuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (GinAppearence)
+
+@end
---
+++ ZooHostel/UIViewController+GinAppearence.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+GinAppearence.m
+//  ZooHostel
+//
+//  Created by Dmitri Doroschuk on 21.07.14.
+//  Copyright (c) 2014 Dmitri Doroschuk. All rights reserved.
+//
+
+#import "UIViewController+GinAppearence.h"
+
+@implementation UIViewController (GinAppearence)
+
+@end
